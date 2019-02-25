25c7170e5ddc5dea3e0b886093b9ecf37ff58b7d
Cody Reed <cody2j2reed@gmail.com>
2019-02-25 16:22:36 -0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+import ResourceList from "./ResourceList";
+
+const App = () => {
+  const [resource, setResource] = useState("posts");
+
+  return (
+    <div className="ui container">
+      <div>
+        <button className="ui button" onClick={() => setResource("posts")}>
+          Posts
+        </button>
+        <button className="ui button" onClick={() => setResource("todos")}>
+          Todos
+        </button>
+      </div>
+      <ResourceList resource={resource} />
+    </div>
+  );
+};
+
+export default App;
